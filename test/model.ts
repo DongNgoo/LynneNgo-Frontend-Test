c06f77a2fd920da92166293793c952b3bec4eb5d
DongN2 <dngo13@myseneca.ca>
2025-04-25 16:30:07 -0400

    feat: create Model Type interface, CreateTodoController, inner function handleAdd, and handleDelete[LynneNgo]

--- test/model.ts
+++ test/model.ts
@@ -1,18 +1,20 @@
 export function createTodoModel() {
-   let todos: string[] = [];
+  // Create the model for the to-do list
+    let todos: string[] = [];
 
     const getTodos = () => todos;
 
+// Add a new todo by pushing it to the todos array
     const addTodo = (text: string) => {
         if(text.trim() !== '') {
         todos.push(text.trim());
         }
     };
-
+//Remove a todo by filtering out the todo with the given index
     const  removeTodo = (index: number) => {
         todos = todos.filter((_, i) => i !== index);
     };
-
+//Reset the todos array
     const reset = () => {
         todos = [];
     };
---
+++ test/output.ts
@@ -0,0 +1,19 @@
+type ModelType = {
+    getTodos: () => string[];
+    addTodo: (text: string) => void;
+    removeTodo: (index: number) => void;
+    reset: () => void;
+}
+// createTodoController take in a model and a setTodos function
+export function createTodoController(model: ModelType, setTodos: (todos: string[]) => void) {
+    const handleAdd = (text: string) => {
+        model.addTodo(text);
+        setTodos(model.getTodos());
+    };
+
+    const handleDelete = (index: number) => {
+        model.removeTodo(index);
+        setTodos([...model.getTodos()]);
+    };
+    
+}
